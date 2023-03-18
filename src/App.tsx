e4dcaf0624238f8a850144d0c91eb9d1fbcb08d3
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-18 10:30:54 +0530

    Content Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Home from "./pages/Home";
 
 export default class App extends Component {
   componentDidMount(): void {
@@ -10,11 +11,7 @@ export default class App extends Component {
   render(): React.ReactNode {
     return (
       <div className="bg-green-500 border-l-pink-700 min-h-screen">
-        <h1>Hello World</h1>
-        <h1 className="text-red-700">Hello World</h1>
-        <h2>Hello World</h2>
-        <h3>Hello World</h3>
-        <h4>Hello World</h4>
+        <Home />
       </div>
     );
   }
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import "./home.css";
+
+//React.Component (Version 16 ekata kalin use krnne mehema)
+export default class Home extends Component {
+  render() {
+    return <div>Home</div>;
+  }
+}
