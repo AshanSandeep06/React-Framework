6787670aece6b3805e6a014f9448b8a8b1b84779
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-18 09:35:34 +0530

    Added some directories to the project

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export default class App extends Component {
-  // componentDidMount(): void {
-  //   // alert("Hello React..!");
-  // }
+  componentDidMount(): void {
+    // alert("Hello React..!");
+  }
 
   render(): React.ReactNode {
     return (
       <div className="bg-green-500 border-l-pink-700 min-h-screen">
         <h1>Hello World</h1>
-        <h1>Hello World</h1>
+        <h1 className="text-red-700">Hello World</h1>
+        <h2>Hello World</h2>
+        <h3>Hello World</h3>
+        <h4>Hello World</h4>
       </div>
     );
   }
