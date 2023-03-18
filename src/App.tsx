d0ab631c04b6ea5cd5678480026568eee18cff6f
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-18 09:10:05 +0530

    Content Updated in tailwind.config.js file

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export default class App extends Component {
-  componentDidMount(): void {
-    // alert("Hello React..!");
-  }
+  // componentDidMount(): void {
+  //   // alert("Hello React..!");
+  // }
 
   render(): React.ReactNode {
     return (
-      <div className="App">
-        <h1>Hello React</h1>
+      <div className="bg-green-500 border-l-pink-700 min-h-screen">
+        <h1>Hello World</h1>
+        <h1>Hello World</h1>
       </div>
     );
   }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js, jsx, ts, tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "abc": {
+          300: '#37306B',
+          200: '#66347F',
+          100: '#865DFF'
+        }
+      }
+    },
+
   },
   plugins: [],
 }
