78948a0ca2cada91febc93bf0691754bf010e9ff
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-25 16:07:32 +0530

    Created Login Component

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,11 +1,58 @@
+import { Button, TextField } from "@mui/material";
 import React, { Component } from "react";
 import "./Contact.css";
 
 export default class Contact extends Component {
   render() {
     return (
-      <div>
-        <h1>Contact</h1>
+      <div className="p-6">
+        <span className="flex flex-col space-y-3 text-center">
+          <h1>Contact Us</h1>
+
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
+            tenetur, quas eaque animi rerum quaerat cumque est mollitia veniam
+            blanditiis facere esse nisi quisquam at reprehenderit enim,
+            doloribus repudiandae? Doloribus? Lorem ipsum dolor sit, amet
+            consectetur adipisicing elit. Inventore tenetur, quas eaque animi
+            rerum quaerat cumque est mollitia veniam blanditiis facere esse nisi
+            quisquam at reprehenderit enim, doloribus repudiandae? Doloribus?
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
+            tenetur, quas eaque animi rerum quaerat cumque est mollitia veniam
+            blanditiis facere esse nisi quisquam at reprehenderit enim,
+            doloribus repudiandae? Doloribus? Lorem ipsum dolor sit, amet
+            consectetur adipisicing elit. Inventore tenetur, quas eaque animi
+            rerum quaerat cumque est mollitia veniam blanditiis facere esse nisi
+            quisquam at reprehenderit enim, doloribus repudiandae? Doloribus?
+          </p>
+        </span>
+
+        <div>
+          <form className="mt-3 w-full py-8 px-32 flex flex-col space-y-3">
+            <TextField
+              label="Email"
+              type="email"
+              variant="outlined"
+              placeholder="Enter Your Email Here"
+              fullWidth={true}
+              required
+            />
+
+            <TextField
+              label="Message"
+              type="text"
+              variant="outlined"
+              placeholder="Enter Your Message Here"
+              fullWidth={true}
+              required
+              multiline
+              minRows={10}
+              maxRows={Infinity}
+            />
+
+            <Button variant="contained">Submit</Button>
+          </form>
+        </div>
       </div>
     );
   }
