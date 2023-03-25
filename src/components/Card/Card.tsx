d15eb9c2c84c59011105829a30e896fbf9affa10
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-25 15:21:19 +0530

    Content Updated

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import AdbIcon from "@mui/icons-material/Adb";
+import AddAlertIcon from "@mui/icons-material/AddAlert";
 
 type CardDetails = {
   id: string;
   title: string;
+  icon: React.ReactNode;
   description: string;
 };
 
@@ -21,6 +25,7 @@ export default class Card extends Component<CardProps, CardState> {
         {
           id: "1",
           title: "Card 1",
+          icon: <AcUnitIcon className="mb-2 text-2xl" />,
           description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex velit, facilis quaerat accusamus tenetur laborum ipsam nemo doloribus sequi mollitia aut ullam dolores tempora quos veritatis quasi aliquid libero ea.",
         },
@@ -28,6 +33,7 @@ export default class Card extends Component<CardProps, CardState> {
         {
           id: "2",
           title: "Card 2",
+          icon: <AdbIcon className="mb-2 text-2xl" />,
           description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex velit, facilis quaerat accusamus tenetur laborum ipsam nemo doloribus sequi mollitia aut ullam dolores tempora quos veritatis quasi aliquid libero ea.",
         },
@@ -35,6 +41,7 @@ export default class Card extends Component<CardProps, CardState> {
         {
           id: "3",
           title: "Card 3",
+          icon: <AddAlertIcon className="mb-2 text-2xl" />,
           description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex velit, facilis quaerat accusamus tenetur laborum ipsam nemo doloribus sequi mollitia aut ullam dolores tempora quos veritatis quasi aliquid libero ea.",
         },
@@ -42,6 +49,7 @@ export default class Card extends Component<CardProps, CardState> {
         {
           id: "4",
           title: "Card 4",
+          icon: <AddAlertIcon className="mb-2 text-2xl" />,
           description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex velit, facilis quaerat accusamus tenetur laborum ipsam nemo doloribus sequi mollitia aut ullam dolores tempora quos veritatis quasi aliquid libero ea.",
         },
@@ -54,7 +62,10 @@ export default class Card extends Component<CardProps, CardState> {
       <div className="grid grid-cols-4 gap-x-5">
         {this.state.cardList.map((card) => (
           <div className="border border-slate-400 px-3 py-5">
-            <h2 className="text-center text-2xl mb-2">{card.title}</h2>
+            <span className="flex flex-col items-center">
+              {card.icon}
+              <h2 className="text-center text-2xl mb-2">{card.title}</h2>
+            </span>
 
             <p className="text-center">{card.description}</p>
           </div>
