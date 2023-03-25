10220a256346c1ea5b43b89e88608ae26f4fa156
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-25 13:35:59 +0530

    Created Card Component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class Card extends Component {
+  render() {
+    return (
+      <div className="border border-slate-400 px-3 py-2">
+        <h2 className="text-center text-2xl">Card 1</h2>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi amet
+          deserunt exercitationem harum, ullam iste sed rem aut eos aspernatur
+          explicabo, nemo nam aliquid consequuntur? Sequi rem accusantium earum
+          alias!
+        </p>
+      </div>
+    );
+  }
+}
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
+import Card from "../../components/Card";
 import "./About.css";
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        <h1>About</h1>
+        <h1 className="text-center mb-8 mt-2">About Us</h1>
+
+        <div className="flex justify-center mb-10">
+          <p className="w-1/2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat esse
+            iure, nulla mollitia, quisquam dolor itaque a sapiente asperiores
+            nemo, deleniti dolore! Explicabo dolorem asperiores saepe, tempore
+            debitis corporis perferendis.
+          </p>
+        </div>
+
+        <div className="flex">
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+        </div>
       </div>
     );
   }
