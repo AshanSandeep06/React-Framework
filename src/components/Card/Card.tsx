fb478a9503ae23181a2e414c6ddb9a424ee62e99
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-25 13:45:00 +0530

    Content Updated

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,18 +1,54 @@
 import React, { Component } from "react";
 
 type CardDetails = {
-    id: string,
-    title: string,
-    description: string
-}
+  id: string;
+  title: string;
+  description: string;
+};
 
 type CardProps = {};
 
 type CardState = {
-    cardList: CardDetails[];
+  cardList: CardDetails[];
 };
 
 export default class Card extends Component<CardProps, CardState> {
+  constructor(props: CardProps) {
+    super(props);
+
+    this.state = {
+      cardList: [
+        {
+          id: "1",
+          title: "Card 1",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex velit, facilis quaerat accusamus tenetur laborum ipsam nemo doloribus sequi mollitia aut ullam dolores tempora quos veritatis quasi aliquid libero ea.",
+        },
+
+        {
+          id: "2",
+          title: "Card 2",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex velit, facilis quaerat accusamus tenetur laborum ipsam nemo doloribus sequi mollitia aut ullam dolores tempora quos veritatis quasi aliquid libero ea.",
+        },
+
+        {
+          id: "3",
+          title: "Card 3",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex velit, facilis quaerat accusamus tenetur laborum ipsam nemo doloribus sequi mollitia aut ullam dolores tempora quos veritatis quasi aliquid libero ea.",
+        },
+
+        {
+          id: "4",
+          title: "Card 4",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex velit, facilis quaerat accusamus tenetur laborum ipsam nemo doloribus sequi mollitia aut ullam dolores tempora quos veritatis quasi aliquid libero ea.",
+        },
+      ],
+    };
+  }
+
   render() {
     return (
       <div className="border border-slate-400 px-3 py-2">
