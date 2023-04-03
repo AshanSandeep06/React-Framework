4b9b61476ea6a9d6bbb0b36d631cbc0a4fc8552e
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-03 19:03:13 +0530

    Content Updated in Profile

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 export default class Profile extends Component {
   render() {
-    return <div>Profile</div>;
+    return (
+      <section id="profile">
+        
+      </section>
+    );
   }
 }
