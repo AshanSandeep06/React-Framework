35b24f5d209a5fa9ad4abe8cef293a3f9c1a2ee8
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-03 18:52:34 +0530

    Content Updated

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,59 +1,56 @@
-import React, { Component } from "react";
-import { Link, NavLink } from "react-router-dom";
-import UserImage from "../../assets/User.png";
+import { NavLink } from "react-router-dom";
+import UserImage from "../../assets/UserImage.png";
 import LogoutIcon from "@mui/icons-material/Logout";
 
-export default class Header extends Component {
-  render() {
-    const activeLink = "border-b-2 cursor-pointer";
-    const normalLink = "cursor-pointer";
+export default function Header() {
+  const activeLink = "border-b-2";
+  const normalLink = "";
 
-    return (
-      <div className="bg-blue-900 text-white fixed h-20 w-full top-0 p-4">
-        <div className="w-full h-full">
-          <div className="flex w-full h-full items-center">
-            <div className="w-1/2">
-              <h1>Feed.com</h1>
-            </div>
-
-            <div className="w-1/2">
-              <div className="flex w-full justify-between font-poppins items-center">
-                <NavLink
-                  to={"/home"}
-                  className={({ isActive }) =>
-                    isActive ? activeLink : normalLink
-                  }
-                >
-                  Home
-                </NavLink>
-                <NavLink
-                  to={"/about"}
-                  className={({ isActive }) =>
-                    isActive ? activeLink : normalLink
-                  }
-                >
-                  About
-                </NavLink>
-                <NavLink
-                  to={"/contact"}
-                  className={({ isActive }) =>
-                    isActive ? activeLink : normalLink
-                  }
-                >
-                  Contact
-                </NavLink>
-                <img
-                  src={UserImage}
-                  alt="UserImage"
-                  className="w-10 h-10 rounded-full"
-                />
-
-                <NavLink to={"/logout"}>{<LogoutIcon />}</NavLink>
-              </div>
-            </div>
-          </div>
-        </div>
+  return (
+    <section
+      id="header"
+      className="flex w-full h-20 bg-blue-800 shadow-lg shadow-black/20 text-white fixed top-0"
+    >
+      <div className="w-1/2 h-full flex items-center gap-2.5 pl-2">
+        <img src={UserImage} alt="UserImage" className="w-10 h-10" />
+        <h1 className="h-max mb-[3px]">My Profile</h1>
       </div>
-    );
-  }
+
+      <div className="w-1/2 h-full pr-5">
+        <ul className="h-full flex items-center justify-end gap-[60px]">
+          <NavLink
+            to={"/Home"}
+            className={({ isActive }) => (isActive ? activeLink : normalLink)}
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            to={"/About"}
+            className={({ isActive }) => (isActive ? activeLink : normalLink)}
+          >
+            About
+          </NavLink>
+
+          <NavLink
+            to={"/Contact"}
+            className={({ isActive }) => (isActive ? activeLink : normalLink)}
+          >
+            Contact
+          </NavLink>
+
+          <NavLink
+            to={"/Profile"}
+            className={({ isActive }) => (isActive ? activeLink : normalLink)}
+          >
+            Profile
+          </NavLink>
+
+          <NavLink to={"/"}>
+            <LogoutIcon />
+          </NavLink>
+        </ul>
+      </div>
+    </section>
+  );
 }
