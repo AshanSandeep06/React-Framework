975d15eec6b1fa7f29c294988a5baf04fccb248d
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-04 19:02:02 +0530

    Created ProfileProps and ProfileState

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import Post from "../../components/Post/Post";
 
+type profileProps = {};
+
+type profileState = {};
+
 export default class Profile extends Component {
   render() {
     return (
       <section id="profile">
         <div className="p-6">
-          
+
         </div>
       </section>
     );
