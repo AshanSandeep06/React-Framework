83a0adf492504712ee51cba9dc15a62bde42beb8
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-04 19:00:57 +0530

    Content Updated

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -6,14 +6,7 @@ export default class Profile extends Component {
     return (
       <section id="profile">
         <div className="p-6">
-          <Post
-            key={"1"}
-            title="Lecture Day 01"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga adipisci quam, doloribus atque alias pariatur sunt ad perspiciatis explicabo, natus molestiae nam dignissimos excepturi, animi sapiente! Voluptates deserunt expedita animi."
-            hoursCount={3}
-            lecturerName="Sanu"
-            tags={["Intro", "React"]}
-          />
+          
         </div>
       </section>
     );
