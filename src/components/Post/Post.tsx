de1b831958a1b87afacce01e4fd48534afe2528b
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-01 18:05:30 +0530

    Content Updated

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -3,6 +3,7 @@ import Tag from "../Tag";
 import DeleteIcon from "@mui/icons-material/Delete";
 
 type PostProps = {
+  _id: string;
   title: string;
   description: string;
   hoursCount?: number;
@@ -11,6 +12,10 @@ type PostProps = {
 };
 
 export default class Post extends Component<PostProps> {
+  deletePost = (postID: string) => {
+
+  };
+
   render() {
     return (
       <div className="p-6 border border-slate-400 w-full rounded mt-3">
@@ -19,12 +24,12 @@ export default class Post extends Component<PostProps> {
 
         <span className="w-full mt-3 flex justify-between">
           <span className="flex space-x-5">
-          {this.props.tags.map((text, index) => (
-            <Tag key={index} tag={text} />
-          ))}
+            {this.props.tags.map((text, index) => (
+              <Tag key={this.props._id} tag={text} />
+            ))}
           </span>
 
-          <button className="">
+          <button onClick={() => this.deletePost(this.props._id)}>
             <DeleteIcon />
           </button>
         </span>
